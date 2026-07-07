1fa7f091fb8213b1eb62d58acbc4ddf1a034db16
havibest <havibest1@gmail.com>
2026-07-07 06:33:02 +0000

    Repository restructure progress

---
+++ client-temp/src/services/referral.service.ts
@@ -0,0 +1,46 @@
+class ReferralService {
+
+  async createReferral() {
+    throw new Error("Not implemented");
+  }
+
+  async getReferral() {
+    throw new Error("Not implemented");
+  }
+
+  async getReferrals() {
+    throw new Error("Not implemented");
+  }
+
+  async updateReferralStatus() {
+    throw new Error("Not implemented");
+  }
+
+  async markEmailVerified() {
+    throw new Error("Not implemented");
+  }
+
+  async markPaymentStarted() {
+    throw new Error("Not implemented");
+  }
+
+  async markPaymentCompleted() {
+    throw new Error("Not implemented");
+  }
+
+  async unlockReward() {
+    throw new Error("Not implemented");
+  }
+
+  async getReferralTimeline() {
+    throw new Error("Not implemented");
+  }
+
+  async getReferralStats() {
+    throw new Error("Not implemented");
+  }
+
+}
+
+export const referralService =
+  new ReferralService();
